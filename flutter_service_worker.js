43b6e22a1217b6d2ce2ec1d2207df6e14de05d9f
Rdot <40841584+rishik4@users.noreply.github.com>
2022-12-26 00:39:21 -0600

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cf936a57f79a5bf80a48cfbafa4c6bd7",
-"/": "cf936a57f79a5bf80a48cfbafa4c6bd7",
-"main.dart.js": "11b9f5069d96780578f9ae1d6c35061b",
+"index.html": "900ca44fedda1700a87f589281152f20",
+"/": "900ca44fedda1700a87f589281152f20",
+"main.dart.js": "b6c0162e06c64a3f6c877e66c476574b",
 "manifest.json": "283861527c3c0163166792436670a869",
 "version.json": "03c8102247419d8da3e52231169f7270"
 };
